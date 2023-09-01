b5905a2164caff290cff64de92218aa64a48f700
bosco0808 <bosco0808@gmail.com>
2023-09-01 18:30:38 +0800

    friend requests now displays existing friend requests also configured some jest

--- app/friendrequests/page.tsx
+++ app/friendrequests/page.tsx
@@ -1,7 +1,53 @@
+"use client";
+import swal from "@/lib/sweetalert";
+import { useEffect } from "react";
+import { useState } from "react";
+import useRedirectIfNotLoggedIn from "@/utils/useredirectifnotloggedin";
+
+interface FriendRequest {
+  id: string;
+  authorId: string;
+  recieverId: string;
+}
+
+interface ResBody {
+  error: boolean;
+  message: string;
+  friendRequests: FriendRequest[] | undefined;
+}
+
 const FriendRequests = () => {
+  useRedirectIfNotLoggedIn();
+  const [friendRequests, setFriendRequests] = useState<FriendRequest[]>([]);
+  useEffect(() => {
+    (async () => {
+      const headerList = new Headers();
+      headerList.set("Authorization", process.env.NEXT_PUBLIC_AUTHKEY || "");
+      const res = await fetch("/api/getfriendrequests", {
+        headers: headerList,
+      });
+      const { error, message, friendRequests }: ResBody = await res.json();
+      if (error) {
+        console.error(message);
+        return;
+      }
+      setFriendRequests(friendRequests || []);
+    })();
+  }, []);
+
   return (
     <>
-      <h1>Friend Requests</h1>
+      <h1 className="m-2">Friend Requests</h1>
+
+      {friendRequests.length !== 0 ? (
+        <ul>
+          {friendRequests.map((friendRequest) => (
+            <li key={friendRequest.id}>{friendRequest.authorId}</li>
+          ))}
+        </ul>
+      ) : (
+        <span className="m-2">No friend requests</span>
+      )}
     </>
   );
 };
